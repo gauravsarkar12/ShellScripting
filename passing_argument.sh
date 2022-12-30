e1d32e48ef4ccf5c5d396de457ba459fb6e80606
Gaurav Sarkar <35233675+gauravsarkar12@users.noreply.github.com>
2022-12-30 13:32:09 +0530

    Add files via upload

---
+++ passing_argument.sh
@@ -0,0 +1,14 @@
+clear
+echo $1 $2 $3 '> echo $1 $2 $3'
+
+args=("$@")
+
+echo $@
+
+echo $#
+
+# Note: To run this program type the file name setps given below:
+#1. Open the terminal.
+#2. Type chmod +x Desktop/passing_argument.sh
+#3. Desktop/passing_argumnet.sh Name1 Name2 Name3 
+#4. Then hit enter.
